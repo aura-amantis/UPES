cab8febaf46a7ce6a5e82f351dc8471c509bb076
Tisha Jain <103413370+tishajain25@users.noreply.github.com>
2023-03-29 11:35:39 +0530

    Simple Package

---
+++ 2nd Year/3rd Sem/OOPs/Exp-6/Q1.java
@@ -0,0 +1,10 @@
+/* Code to write a Java program to implement the concept of importing classes from user 
+defined packages and created packages. */
+
+
+package UPES.NIT.IIT ;  
+public class Simple{  
+ public static void main(String args[]){  
+    System.out.println("Welcome to package");  
+   }  
+}  
